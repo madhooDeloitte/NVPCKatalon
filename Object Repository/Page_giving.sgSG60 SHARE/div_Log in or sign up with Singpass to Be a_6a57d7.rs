c8fce641a31f069fac9ebaccf7f8c09538143f1b
kavinDeloitte <bkavinraaj@deloitte.com>
2025-01-03 18:14:54 +0800

    Update div_Log in or sign up with Singpass to Be a_6a57d7.rs

--- Object Repository/Page_giving.sgSG60 SHARE/div_Log in or sign up with Singpass to Be a_6a57d7.rs
+++ Object Repository/Page_giving.sgSG60 SHARE/div_Log in or sign up with Singpass to Be a_6a57d7.rs
@@ -3,7 +3,7 @@
    <description></description>
    <name>div_Log in or sign up with Singpass to Be a_6a57d7</name>
    <tag></tag>
-<<<<<<< HEAD
+
    <elementGuidId>8c1445f9-03f3-4704-9e21-5ddb16bc8137</elementGuidId>
    <selectorCollection>
       <entry>
@@ -81,102 +81,6 @@
       <type>Main</type>
       <value>//div[(text() = 'Log in or sign up with Singpass to:	• Be able to claim tax deduction	• Make monthly donations' or . = 'Log in or sign up with Singpass to:	• Be able to claim tax deduction	• Make monthly donations')]</value>
       <webElementGuid>ba66a859-0157-430c-b341-24a0f3585729</webElementGuid>
-=======
 
-   <elementGuidId>8c1445f9-03f3-4704-9e21-5ddb16bc8137</elementGuidId>
- 
-   <selectorCollection>
-      <entry>
-         <key>XPATH</key>
-         <value>//div[@id='b2-$b3']/div</value>
-      </entry>
-      <entry>
-         <key>CSS</key>
-         <value>div.alert-box.alert-info</value>
-      </entry>
-   </selectorCollection>
-   <selectorMethod>XPATH</selectorMethod>
-   <smartLocatorEnabled>false</smartLocatorEnabled>
-   <useRalativeImagePath>true</useRalativeImagePath>
-   <webElementProperties>
-      <isSelected>false</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>tag</name>
-      <type>Main</type>
-      <value>div</value>
-
-      <webElementGuid>ca87825e-a470-4f43-a72b-6643e62ab89b</webElementGuid>
-
-   </webElementProperties>
-   <webElementProperties>
-      <isSelected>false</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>class</name>
-      <type>Main</type>
-      <value>alert-box alert-info</value>
-
-      <webElementGuid>3601761b-046d-49d6-a921-a8d631006661</webElementGuid>
-
-   </webElementProperties>
-   <webElementProperties>
-      <isSelected>true</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>text</name>
-      <type>Main</type>
-      <value>Log in or sign up with Singpass to:	• Be able to claim tax deduction	• Make monthly donations</value>
-
-      <webElementGuid>0e788f01-b965-4d8f-92cf-4bae49cbd6ce</webElementGuid>
-
-   </webElementProperties>
-   <webElementProperties>
-      <isSelected>false</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>xpath</name>
-      <type>Main</type>
-      <value>id(&quot;b2-$b3&quot;)/div[@class=&quot;alert-box alert-info&quot;]</value>
-
-      <webElementGuid>edfc4f06-b0ea-4af2-8a2d-988c64573442</webElementGuid>
-
-   </webElementProperties>
-   <webElementXpaths>
-      <isSelected>true</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>xpath:idRelative</name>
-      <type>Main</type>
-      <value>//div[@id='b2-$b3']/div</value>
-
-      <webElementGuid>62bdacc0-bbc1-482e-9e48-a5cde1a04129</webElementGuid>
-
-   </webElementXpaths>
-   <webElementXpaths>
-      <isSelected>false</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>xpath:neighbor</name>
-      <type>Main</type>
-      <value>(.//*[normalize-space(text()) and normalize-space(.)='Run by the Community Chest'])[1]/following::div[10]</value>
-
-      <webElementGuid>f8ffaba8-d39d-49f3-9165-ae83284b7ce3</webElementGuid>
-
-   </webElementXpaths>
-   <webElementXpaths>
-      <isSelected>false</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>xpath:position</name>
-      <type>Main</type>
-      <value>//div[2]/div/div[2]/div/div/div/div</value>
-
-      <webElementGuid>056cf816-89fe-4086-8ec4-27bf3455b010</webElementGuid>
-
-   </webElementXpaths>
-   <webElementXpaths>
-      <isSelected>false</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>xpath:customAttributes</name>
-      <type>Main</type>
-      <value>//div[(text() = 'Log in or sign up with Singpass to:	• Be able to claim tax deduction	• Make monthly donations' or . = 'Log in or sign up with Singpass to:	• Be able to claim tax deduction	• Make monthly donations')]</value>
-
-      <webElementGuid>ba66a859-0157-430c-b341-24a0f3585729</webElementGuid>
-
->>>>>>> branch 'UATSG60' of https://github.com/madhooDeloitte/NVPCKatalon
    </webElementXpaths>
 </WebElementEntity>
