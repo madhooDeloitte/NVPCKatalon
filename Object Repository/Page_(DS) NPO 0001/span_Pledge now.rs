670067088c7393e2dd405ccd10d6b6fa794f1602
kavinDeloitte <bkavinraaj@deloitte.com>
2025-01-03 17:55:41 +0800

    Update span_Pledge now.rs

--- Object Repository/Page_(DS) NPO 0001/span_Pledge now.rs
+++ Object Repository/Page_(DS) NPO 0001/span_Pledge now.rs
@@ -6,7 +6,7 @@
    <elementGuidId>01adf133-22a9-4449-a09f-2604cf3cf6db</elementGuidId>
    <selectorCollection>
       <entry>
-<<<<<<< HEAD
+
          <key>BASIC</key>
          <value>//*[(text() = 'Pledge now' or . = 'Pledge now')]</value>
       </entry>
@@ -101,93 +101,6 @@
       <type>Main</type>
       <value>//span[(text() = 'Pledge now' or . = 'Pledge now')]</value>
       <webElementGuid>7ef02614-612e-4804-9c19-71543aac023d</webElementGuid>
-=======
-         <key>CSS</key>
-         <value>button.btn.btn-primary > span</value>
-      </entry>
-      <entry>
-         <key>XPATH</key>
-         <value>//div[@id='b2-b15-b21-b1-PrimaryButtons']/button/span</value>
-      </entry>
-      <entry>
-         <key>BASIC</key>
-         <value>//*[(text() = 'Pledge now' or . = 'Pledge now')]</value>
-      </entry>
-   </selectorCollection>
-   <selectorMethod>BASIC</selectorMethod>
-   <smartLocatorEnabled>false</smartLocatorEnabled>
-   <useRalativeImagePath>true</useRalativeImagePath>
-   <webElementProperties>
-      <isSelected>false</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>tag</name>
-      <type>Main</type>
-      <value>span</value>
-      <webElementGuid>85b95749-4a15-4760-97bb-39ca59586cc8</webElementGuid>
-   </webElementProperties>
-   <webElementProperties>
-      <isSelected>true</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>text</name>
-      <type>Main</type>
-      <value>Pledge now</value>
-      <webElementGuid>e8548152-f238-4c3b-ab1f-22506a0f4a39</webElementGuid>
-   </webElementProperties>
-   <webElementProperties>
-      <isSelected>false</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>xpath</name>
-      <type>Main</type>
-      <value>id(&quot;b2-b15-b21-b1-PrimaryButtons&quot;)/button[@class=&quot;btn btn-primary&quot;]/span[1]</value>
-      <webElementGuid>103ae101-fe42-4df5-90ed-e149dc176176</webElementGuid>
-   </webElementProperties>
-   <webElementXpaths>
-      <isSelected>true</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>xpath:idRelative</name>
-      <type>Main</type>
-      <value>//div[@id='b2-b15-b21-b1-PrimaryButtons']/button/span</value>
-      <webElementGuid>4acc2171-bca5-4f1c-bbb2-f5c11c5e62e8</webElementGuid>
-   </webElementXpaths>
-   <webElementXpaths>
-      <isSelected>false</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>xpath:neighbor</name>
-      <type>Main</type>
-      <value>(.//*[normalize-space(text()) and normalize-space(.)='Cancel'])[1]/following::span[1]</value>
-      <webElementGuid>f6dc441f-4b60-4135-938d-4f8119313494</webElementGuid>
-   </webElementXpaths>
-   <webElementXpaths>
-      <isSelected>false</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>xpath:neighbor</name>
-      <type>Main</type>
-      <value>(.//*[normalize-space(text()) and normalize-space(.)='Test question to ask'])[1]/following::span[2]</value>
-      <webElementGuid>8d977634-7989-4c3a-863e-11974e7115ad</webElementGuid>
-   </webElementXpaths>
-   <webElementXpaths>
-      <isSelected>false</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>xpath:neighbor</name>
-      <type>Main</type>
-      <value>//*/text()[normalize-space(.)='Pledge now']/parent::*</value>
-      <webElementGuid>97b1eee3-f77f-41e7-b595-b161c56d4ffa</webElementGuid>
-   </webElementXpaths>
-   <webElementXpaths>
-      <isSelected>false</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>xpath:position</name>
-      <type>Main</type>
-      <value>//button/span</value>
-      <webElementGuid>3f3b7844-20ac-4eae-a56b-e32864f0fe98</webElementGuid>
-   </webElementXpaths>
-   <webElementXpaths>
-      <isSelected>false</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>xpath:customAttributes</name>
-      <type>Main</type>
-      <value>//span[(text() = 'Pledge now' or . = 'Pledge now')]</value>
-      <webElementGuid>d03666ee-8c63-43e6-bc4c-d8b7f1cf343f</webElementGuid>
->>>>>>> branch 'UATSG60' of https://github.com/madhooDeloitte/NVPCKatalon
+
    </webElementXpaths>
 </WebElementEntity>
