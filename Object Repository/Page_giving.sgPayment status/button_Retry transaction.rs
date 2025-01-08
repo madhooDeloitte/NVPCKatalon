3abc7ea64f436853a39d45f9ac2fc8fe7c5b19ba
SeanpowerDeloitte <sepower@deloitte.com>
2025-01-08 11:06:24 +0800

    revised 08/01/2025

--- Object Repository/Page_giving.sgPayment status/button_Retry transaction.rs
+++ Object Repository/Page_giving.sgPayment status/button_Retry transaction.rs
@@ -6,13 +6,10 @@
    <elementGuidId>b010f8f3-73b7-4d0f-998e-65ed6bfe718b</elementGuidId>
    <selectorCollection>
       <entry>
-<<<<<<< HEAD
          <key>BASIC</key>
          <value>//button[@class = 'btn btn-primary' and @type = 'button' and (text() = 'Retry transaction' or . = 'Retry transaction')]</value>
       </entry>
       <entry>
-=======
->>>>>>> branch 'master' of https://github.com/madhooDeloitte/NVPCKatalon
          <key>XPATH</key>
          <value>(//button[@type='button'])[3]</value>
       </entry>
@@ -40,7 +37,6 @@
       <name>tag</name>
       <type>Main</type>
       <value>button</value>
-<<<<<<< HEAD
       <webElementGuid>aba21db3-cc1f-41a9-b805-707a607d4692</webElementGuid>
    </webElementProperties>
    <webElementProperties>
@@ -58,25 +54,6 @@
       <type>Main</type>
       <value>button</value>
       <webElementGuid>136387aa-0422-414a-b568-34905363966b</webElementGuid>
-=======
-      <webElementGuid>a7641304-f6b3-4f34-b401-a49a18497bd1</webElementGuid>
-   </webElementProperties>
-   <webElementProperties>
-      <isSelected>false</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>class</name>
-      <type>Main</type>
-      <value>btn btn-primary</value>
-      <webElementGuid>54aaf027-63be-4bf9-aff1-ea2d2cccda21</webElementGuid>
-   </webElementProperties>
-   <webElementProperties>
-      <isSelected>false</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>type</name>
-      <type>Main</type>
-      <value>button</value>
-      <webElementGuid>3f4b8bc4-017b-48dd-bac2-09a65e2676a6</webElementGuid>
->>>>>>> branch 'master' of https://github.com/madhooDeloitte/NVPCKatalon
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -84,11 +61,7 @@
       <name>ktl-id</name>
       <type>Main</type>
       <value>btnRetryTran</value>
-<<<<<<< HEAD
       <webElementGuid>4b1a3a28-34a9-4b9d-8a79-2d41464fb698</webElementGuid>
-=======
-      <webElementGuid>30560209-1e7a-426d-a036-9c5130aedb42</webElementGuid>
->>>>>>> branch 'master' of https://github.com/madhooDeloitte/NVPCKatalon
    </webElementProperties>
    <webElementProperties>
       <isSelected>true</isSelected>
@@ -96,11 +69,7 @@
       <name>text</name>
       <type>Main</type>
       <value>Retry transaction</value>
-<<<<<<< HEAD
       <webElementGuid>cacf277a-d92b-4853-98db-d76854a77747</webElementGuid>
-=======
-      <webElementGuid>03b913ff-21b5-49d3-9212-f67c0e74c4ec</webElementGuid>
->>>>>>> branch 'master' of https://github.com/madhooDeloitte/NVPCKatalon
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -108,11 +77,7 @@
       <name>xpath</name>
       <type>Main</type>
       <value>id(&quot;b2-b3-b6-Right&quot;)/div[@class=&quot;margin-top-m text-align-right&quot;]/button[@class=&quot;btn btn-primary&quot;]</value>
-<<<<<<< HEAD
       <webElementGuid>0e17210c-d0a0-468c-b3ab-62389ce46f5e</webElementGuid>
-=======
-      <webElementGuid>d5c9cce9-1c5a-4d6c-b6b9-ff4e978ba4b9</webElementGuid>
->>>>>>> branch 'master' of https://github.com/madhooDeloitte/NVPCKatalon
    </webElementProperties>
    <webElementXpaths>
       <isSelected>true</isSelected>
@@ -120,11 +85,7 @@
       <name>xpath:attributes</name>
       <type>Main</type>
       <value>(//button[@type='button'])[3]</value>
-<<<<<<< HEAD
       <webElementGuid>3b90a9b3-e1e8-48f3-91e2-6a9c04a71013</webElementGuid>
-=======
-      <webElementGuid>46c3d803-bd8f-4607-aac8-07ba47f3a318</webElementGuid>
->>>>>>> branch 'master' of https://github.com/madhooDeloitte/NVPCKatalon
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -132,11 +93,7 @@
       <name>xpath:idRelative</name>
       <type>Main</type>
       <value>//div[@id='b2-b3-b6-Right']/div/button</value>
-<<<<<<< HEAD
       <webElementGuid>3dd9651e-2c73-4cad-ad70-4d3d3277fe73</webElementGuid>
-=======
-      <webElementGuid>dc2c5b25-f3ec-40d7-85ff-e9baf2a433bb</webElementGuid>
->>>>>>> branch 'master' of https://github.com/madhooDeloitte/NVPCKatalon
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -144,11 +101,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='e6237e23-5db9-4b9a-9198-e7ce8d813e13'])[1]/following::button[1]</value>
-<<<<<<< HEAD
       <webElementGuid>fa45990e-888d-4291-985f-3257c1dda4a7</webElementGuid>
-=======
-      <webElementGuid>60ee41b3-97a2-408c-b0ec-7a32dca59d99</webElementGuid>
->>>>>>> branch 'master' of https://github.com/madhooDeloitte/NVPCKatalon
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -156,11 +109,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='Reference'])[1]/following::button[1]</value>
-<<<<<<< HEAD
       <webElementGuid>e871dfe9-e4fd-4560-a5a3-4dd9f73f41b5</webElementGuid>
-=======
-      <webElementGuid>4be5fc80-ff92-49b1-b906-263f1328ac2d</webElementGuid>
->>>>>>> branch 'master' of https://github.com/madhooDeloitte/NVPCKatalon
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -168,11 +117,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='Organised by'])[1]/preceding::button[1]</value>
-<<<<<<< HEAD
       <webElementGuid>78ba369f-574c-412d-b29e-fd6ca4f56338</webElementGuid>
-=======
-      <webElementGuid>f5b56f1a-8e67-4888-90d9-af78e0784ded</webElementGuid>
->>>>>>> branch 'master' of https://github.com/madhooDeloitte/NVPCKatalon
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -180,11 +125,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='Together, because'])[1]/preceding::button[1]</value>
-<<<<<<< HEAD
       <webElementGuid>1024847d-2d61-4bcb-a4db-76a6e1423696</webElementGuid>
-=======
-      <webElementGuid>5002d707-7480-43d3-9b4b-5e0b903a73b3</webElementGuid>
->>>>>>> branch 'master' of https://github.com/madhooDeloitte/NVPCKatalon
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -192,11 +133,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>//*/text()[normalize-space(.)='Retry transaction']/parent::*</value>
-<<<<<<< HEAD
       <webElementGuid>afa60539-fd86-45c8-8443-8fdf8a78942a</webElementGuid>
-=======
-      <webElementGuid>e1ea5429-71e5-4352-9105-7a07939eba4b</webElementGuid>
->>>>>>> branch 'master' of https://github.com/madhooDeloitte/NVPCKatalon
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -204,23 +141,14 @@
       <name>xpath:position</name>
       <type>Main</type>
       <value>//div[2]/div/button</value>
-<<<<<<< HEAD
       <webElementGuid>0c8a51fd-17ec-4281-962e-daaee725803a</webElementGuid>
-=======
-      <webElementGuid>79707899-9e58-4a3a-a4ec-627a746a40d4</webElementGuid>
->>>>>>> branch 'master' of https://github.com/madhooDeloitte/NVPCKatalon
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:customAttributes</name>
       <type>Main</type>
-<<<<<<< HEAD
       <value>//button[@type = 'button' and (text() = 'Retry transaction' or . = 'Retry transaction')]</value>
       <webElementGuid>71d65406-c5c1-4c24-997f-bb1a898da2ac</webElementGuid>
-=======
-      <value>//button[@type = 'button' and (text() = 'Retry transaction' or . = 'Retry transaction')]</value>
-      <webElementGuid>1d45cf2e-c6ff-4e91-afa2-8d8423aa74f5</webElementGuid>
->>>>>>> branch 'master' of https://github.com/madhooDeloitte/NVPCKatalon
    </webElementXpaths>
 </WebElementEntity>
